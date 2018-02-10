9e93870041c40f336a27243721a8e6d9e54ac97b
Jdyn <jaden.smith5389@gmail.com>
2018-02-09 19:48:55 -0800

    Initial Push
    Skeleton (UI)

---
+++ Academus/Academus/Controller/PlannerVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Academus
+//
+//  Created by Jaden Moore on 2/9/18.
+//  Copyright © 2018 Caffeinated Insomniacs. All rights reserved.
+//
+
+import UIKit
+
+class PlannerVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
---
+++ Academus/Academus/Services/AuthService.swift
@@ -0,0 +1,9 @@
+//
+//  AuthService.swift
+//  Academus
+//
+//  Created by Jaden Moore on 2/9/18.
+//  Copyright © 2018 Caffeinated Insomniacs. All rights reserved.
+//
+
+import Foundation
