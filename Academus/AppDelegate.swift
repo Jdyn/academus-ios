ff6798dbbb26930a6554039ac3b4d0935bb7de95
Pasha Bou <pashade1@yahoo.com>
2018-04-11 11:40:21 -0700

    test

--- Academus/AppDelegate.swift
+++ Academus/AppDelegate.swift
@@ -63,6 +63,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         return true
     }
     
+    func application(received remoteMessage: MessagingRemoteMessage) {
+        print(remoteMessage.appData)
+    }
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.alert, .sound])
     }
