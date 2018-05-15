2e30fe13000689ac49fa2dcd71dceefdeb4a0603
TUGenius <pashade1@yahoo.com>
2018-05-15 16:37:32 -0700

    ZIP Code Prompt

--- Academus/Controllers/Account/IntegrationSearchController.swift
+++ Academus/Controllers/Account/IntegrationSearchController.swift
@@ -9,11 +9,6 @@
 import UIKit
 import CoreLocation
 
-enum SearchMode {
-    case zip
-    case filter
-}
-
 class IntegrationSearchController: UITableViewController, IntegrationSearchDelegate {
     
     var integration: IntegrationChoice?
@@ -23,17 +18,6 @@ class IntegrationSearchController: UITableViewController, IntegrationSearchDeleg
     
     var filtered = [IntegrationResult]()
     var results = [IntegrationResult]()
-    var searchMode = SearchMode.zip {
-        didSet {
-            if searchMode == .zip {
-                navigationItem.searchController?.searchBar.setImage(#imageLiteral(resourceName: "location"), for: .search, state: .normal)
-                navigationItem.searchController?.searchBar.placeholder = "ZIP Code"
-            } else {
-                navigationItem.searchController?.searchBar.setImage(#imageLiteral(resourceName: "search"), for: .search, state: .normal)
-                navigationItem.searchController?.searchBar.placeholder = "Filter Districts"
-            }
-        }
-    }
     var locationManager = CLLocationManager()
     let integrationCellID = "SearchIntegrationCell"
     
@@ -48,6 +32,9 @@ class IntegrationSearchController: UITableViewController, IntegrationSearchDeleg
         tableView.separatorStyle = .none
         tableView.contentInset = UIEdgeInsetsMake(0, 0, 55, 0)
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "location"), style: .plain, target: self, action: #selector(zipPrompt))
+        navigationItem.rightBarButtonItem?.tintColor = .navigationsGreen
+        
         let searchController = UISearchController(searchResultsController: nil)
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.dimsBackgroundDuringPresentation = false
@@ -55,8 +42,7 @@ class IntegrationSearchController: UITableViewController, IntegrationSearchDeleg
         searchController.searchBar.delegate = self
         searchController.searchBar.barStyle = .black
         searchController.searchBar.tintColor = .white
-        searchController.searchBar.placeholder = "ZIP Code"
-        searchController.searchBar.setImage(#imageLiteral(resourceName: "location"), for: .search, state: .normal)
+        searchController.searchBar.placeholder = "Filter Districts"
         searchController.searchBar.enablesReturnKeyAutomatically = false
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
@@ -79,6 +65,70 @@ class IntegrationSearchController: UITableViewController, IntegrationSearchDeleg
     func didGetResults(results: [IntegrationResult]) {
         self.filtered = results
         self.results = results
+    }
+    
+    @objc func zipPrompt() {
+        let ac = UIAlertController(title: "", message: "", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let searchAction = UIAlertAction(title: "Search", style: .default) { _ in
+            self.handleZIP(ac.textFields?.first?.text ?? "")
+        }
+        
+        let attributesTitle = [NSAttributedStringKey.foregroundColor: UIColor.white,
+                               NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 17)]
+        let attributesMessage = [NSAttributedStringKey.foregroundColor: UIColor.white,
+                                 NSAttributedStringKey.font: UIFont.systemFont(ofSize: 13)]
+        let attributedTitle = NSMutableAttributedString(string: "Find Your District", attributes: attributesTitle)
+        let attributedMessage = NSMutableAttributedString(string: "Search by ZIP Code", attributes: attributesMessage)
+        ac.setValue(attributedTitle, forKey: "attributedTitle")
+        ac.setValue(attributedMessage, forKey: "attributedMessage")
+        
+        ac.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = .navigationsMediumGrey
+        ac.view.tintColor = .white
+        ac.addTextField { $0.placeholder = "ZIP Code"; $0.tintColor = .black }
+        ac.addAction(cancelAction)
+        ac.addAction(searchAction)
+        
+        present(ac, animated: true)
+    }
+    
+    @objc func handleZIP(_ text: String) {
+        guard !text.isEmpty else { return }
+        filtered = [IntegrationResult]()
+        tableView.reloadData()
+        
+        let ai = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        ai.color = .navigationsGreen
+        ai.startAnimating()
+        self.tableView.backgroundView = ai
+        
+        guard Int(text) != nil else {
+            let label = UILabel().setUpLabel(text: "Please enter a valid ZIP Code.", font: UIFont.standard!, fontColor: .white)
+            label.numberOfLines = 0
+            label.textAlignment = .center
+            self.tableView.backgroundView = label
+            self.filtered = [IntegrationResult]()
+            self.results = [IntegrationResult]()
+            self.navigationItem.hidesSearchBarWhenScrolling = false
+            return
+        }
+        
+        integrationService?.searchIntegrations(for: text, completion: { (success, error) in
+            if success {
+                self.tableView.backgroundView = nil
+                self.navigationItem.hidesSearchBarWhenScrolling = true
+            } else {
+                let label = UILabel().setUpLabel(text: error ?? "Please check your Internet connection.", font: UIFont.standard!, fontColor: .white)
+                label.numberOfLines = 0
+                label.textAlignment = .center
+                self.tableView.backgroundView = label
+                self.filtered = [IntegrationResult]()
+                self.results = [IntegrationResult]()
+                self.navigationItem.hidesSearchBarWhenScrolling = false
+            }
+            
+            self.tableView.reloadData()
+        })
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return filtered.count }
@@ -116,7 +166,7 @@ extension IntegrationSearchController: CLLocationManagerDelegate {
         CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
             if error == nil, let placemark = placemarks?.first, let zip = placemark.postalCode {
                 manager.stopUpdatingLocation()
-                self.navigationItem.searchController?.searchBar.text = zip
+                self.handleZIP(zip)
             }
         })
     }
@@ -126,61 +176,23 @@ extension IntegrationSearchController: UISearchBarDelegate, UISearchResultsUpdat
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
         navigationItem.searchController?.resignFirstResponder()
-        searchBar.text = (searchMode == .filter) ? searchBar.text : nil
-        searchMode = .filter
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
         navigationItem.searchController?.resignFirstResponder()
-        searchMode = .zip
         filtered = results
         tableView.reloadData()
     }
     
     func updateSearchResults(for searchController: UISearchController) {
-        if searchMode == .filter {
-            guard let text = searchController.searchBar.text, !text.isEmpty else {
-                filtered = results
-                tableView.reloadData()
-                return
-            }
-            
-            filtered = results.filter { $0.name?.lowercased().contains(text.lowercased()) == true }
+        guard let text = searchController.searchBar.text, !text.isEmpty else {
+            filtered = results
             tableView.reloadData()
-        } else {
-            if let text = searchController.searchBar.text, text.count > 4 {
-                if filtered.isEmpty {
-                    let ai = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
-                    ai.color = .navigationsGreen
-                    ai.startAnimating()
-                    self.tableView.backgroundView = ai
-                }
-                
-                integrationService?.searchIntegrations(for: text, completion: { (success, error) in
-                    if success {
-                        self.tableView.backgroundView = nil
-                        self.navigationItem.hidesSearchBarWhenScrolling = true
-                        
-                        if !searchController.searchBar.isFirstResponder {
-                            self.navigationItem.hidesSearchBarWhenScrolling = false
-                            searchController.searchBar.text = (self.searchMode == .filter) ? searchController.searchBar.text : nil
-                            self.searchMode = .filter
-                        }
-                        
-                    } else {
-                        let label = UILabel().setUpLabel(text: error ?? "Please check your Internet connection.", font: UIFont.standard!, fontColor: .white)
-                        label.numberOfLines = 0
-                        label.textAlignment = .center
-                        self.tableView.backgroundView = label
-                        self.filtered = [IntegrationResult]()
-                        self.results = [IntegrationResult]()
-                        self.navigationItem.hidesSearchBarWhenScrolling = false
-                    }
-                    
-                    self.tableView.reloadData()
-                })
-            }
+            return
         }
+        
+        filtered = results.filter { $0.name?.lowercased().contains(text.lowercased()) == true }
+        tableView.reloadData()
     }
 }
